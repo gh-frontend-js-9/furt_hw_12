177ced4ffe71a15934a43a6d8285a74f06a16bc3
Karina1213 <karinaa121213@gmail.com>
2020-01-14 02:48:20 +0200

    add validate form

--- src/assets/scripts/send-message.js
+++ src/assets/scripts/send-message.js
@@ -3,7 +3,7 @@ import {cloudMyMessage} from './common.js';
 async function sendMessage(e) {
     let message = document.getElementById('textarea').value;
     e.preventDefault();
-    const url = 'http://localhost:3000/api/threads/messages';
+    let url = 'http://localhost:3000/api/threads/messages';
     let dataRaw = {
         "thread": {
             "_id": "5e18780b0a7a824ce07bf0a1"
@@ -29,17 +29,40 @@ async function sendMessage(e) {
             throw new Error(`No connection ${url}, status: ${response.status}`);
         }
         let token = await response.headers.get('x-auth-token');
-         cloudMyMessage(message);
+        cloudMyMessage(message);
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
 function setOnClickByElemId(elemId, callback) {
-        let sendButton = document.getElementById(elemId);
-         sendButton.addEventListener('click', callback);
+    let sendButton = document.getElementById(elemId);
+    sendButton.addEventListener('click', callback);
 }
 setOnClickByElemId('submit', sendMessage)
 
+let disabledButton = document.getElementById("submit")
+disabledButton.disabled = true;
+function validateForm() {
+    const messageForm =  "messageForm";
+    const messageTextarea = "messageTextarea";
+    let empty = document.forms[messageForm][messageTextarea].value;
+    if (empty == '') {
+        disabledButton.disabled = true;
+     }
+    else{
+        disabledButton.removeAttribute('disabled')
+    }
+}
+setOnClickByElemId('submit', validateForm);
 
+let writeMessage = document.getElementById('textarea');
+writeMessage.oninput = function(){
+    validateForm()
+}
+
+function cleanTextarea(){
+    document.getElementById('textarea').value = "";
+}
+setOnClickByElemId('submit', cleanTextarea);
 
