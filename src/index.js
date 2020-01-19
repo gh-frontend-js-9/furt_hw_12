77e4b67400bf75c9a171f5ad8ac81de7115da141
Karina1213 <karinaa121213@gmail.com>
2020-01-19 16:21:22 +0200

    updated API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './assets/scripts/common';
 
-import './assets/scripts/get-all-threads';
-import './assets/scripts/start-thread';
-import './assets/scripts/get-thread-messages';
 import './assets/scripts/send-message';
+import './assets/scripts/retrieve-all-threads';
+import './assets/scripts/start-thread';
+import './assets/scripts/retrieve-all-thread-messages';
 
 import './assets/styles/scss/main.scss';
 
