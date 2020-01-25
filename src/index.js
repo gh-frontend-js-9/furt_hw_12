d8c9657d9e13e8f53e03da55c4ce3283eb56f572
Karina1213 <karinaa121213@gmail.com>
2020-01-25 20:07:18 +0200

    index.js add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import './assets/scripts/common';
-
-import './assets/scripts/send-message';
-import './assets/scripts/retrieve-all-threads';
-import './assets/scripts/start-thread';
-import './assets/scripts/retrieve-all-thread-messages';
+import './assets/scripts/messages/retrieve-all-threads';
+import './assets/scripts/messages/start-thread';
+import './assets/scripts/messages/retrieve-all-thread-messages';
+import './assets/scripts/messages/common';
+import './assets/scripts/messages/get-user-by-id';
 
 import './assets/styles/scss/main.scss';
 
